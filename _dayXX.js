2679aa618ee321d791ca53421c74a6e4566c229b
Matt Braddock <matt.braddock@gmail.com>
2021-11-12 13:08:47 -0500

    initial setup

---
+++ _dayXX.js
@@ -0,0 +1,16 @@
+// data to test
+// part one
+const input = '';
+// part two, if necessary
+//const input = '';
+
+// actual data (map at the end if necessary)
+//const input =  require('fs').readFileSync(require('path').resolve(__dirname, './inputs/dayXX'), 'utf-8').split('\n').filter(Boolean);
+
+// part one work
+let solutionOne;
+console.log(`Part One...\nSolution: ${solutionOne}`);
+
+// part two work
+let solutionTwo;
+//console.log(`Part Two...\nSolution: ${solutionTwo}`);
