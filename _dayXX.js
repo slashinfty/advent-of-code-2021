9736616c8c0ef091f499735a3d769ffe086597f4
Matt Braddock <matt.braddock@gmail.com>
2021-12-01 09:27:21 -0500

    added task statement

--- _dayXX.js
+++ _dayXX.js
@@ -5,11 +5,13 @@ let input;
 //const input =  require('fs').readFileSync(require('path').resolve(__dirname, `./inputs/${require('path').basename(__filename).replace('.js', '')}`), 'utf-8').split('\n').filter(Boolean);
 
 // part one work
+// task: 
 // expected outcome for test: 
 let solutionOne;
 console.log(`Part One...\nSolution: ${solutionOne}`);
 
 // part two work
+// task: 
 // expected outcome for test: 
 let solutionTwo;
 console.log(`\nPart Two...\nSolution: ${solutionTwo}`);
--- day01.js
+++ day01.js
@@ -5,11 +5,13 @@
 const input =  require('fs').readFileSync(require('path').resolve(__dirname, `./inputs/${require('path').basename(__filename).replace('.js', '')}`), 'utf-8').split('\n').filter(Boolean).map(e => parseInt(e));
 
 // part one work
+// task: count the number of times a depth measurement increases from the previous measurement.
 // expected outcome for test: 7
 let solutionOne = input.reduce((a, b, i) => a += i > 0 && b > input[i - 1], 0);
 console.log(`Part One...\nSolution: ${solutionOne}`);
 
 // part two work
+// task: consider sums of a three-measurement sliding window, and count the number of times the sum of measurements in this sliding window increases from the previous sum
 // expected outcome for test: 5
 let solutionTwo = input.reduce((a, b, i) => a += i > 2 && b > input[i - 3], 0);
 console.log(`\nPart Two...\nSolution: ${solutionTwo}`);
