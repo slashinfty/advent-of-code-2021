2942e52fccd217daaa8602ef0a6e4f19934d832d
Matt Braddock <matt.braddock@gmail.com>
2021-12-10 07:21:39 -0500

    day 10 complete

--- day10.js
+++ day10.js
@@ -19,10 +19,11 @@ const input =  require('fs').readFileSync(require('path').resolve(__dirname, `./
 // task: 
 // expected outcome for test: 26397
 let corruptCharacters = [];
-let incompleteLines = [];
+let incompleteScores = [];
 input.forEach(x => {
     let inputs = x.split('');
     let opens = [];
+    let corrupt = false;
     for (let i = 0; i < inputs.length; i++) {
         if (/[\(\[\{\<]/.test(inputs[i])) opens.push(inputs[i]);
         else {
@@ -35,9 +36,21 @@ input.forEach(x => {
             ) continue;
             else {
                 corruptCharacters.push(inputs[i]);
+                corrupt = true;
                 break;
             }
         }
+    }
+    if (!corrupt) {
+        let score = 0;
+        for (let i = opens.length - 1; i > -1; i--) {
+            score *= 5;
+            if (opens[i] === '(') score += 1;
+            else if (opens[i] === '[') score += 2;
+            else if (opens[i] === '{') score += 3;
+            else if (opens[i] === '<') score += 4;
+        }
+        incompleteScores.push(score);
     }
 });
 let solutionOne = corruptCharacters.reduce((a, b) => a += b === ')' ? 3 : b === ']' ? 57 : b === '}' ? 1197 : 25137, 0);
@@ -45,6 +58,6 @@ console.log(`Part One...\nSolution: ${solutionOne}`);
 
 // part two work
 // task: 
-// expected outcome for test: 
-let solutionTwo;
+// expected outcome for test: 288957
+let solutionTwo = incompleteScores.sort((a, b) => b - a)[(incompleteScores.length - 1) / 2];
 console.log(`\nPart Two...\nSolution: ${solutionTwo}`);
